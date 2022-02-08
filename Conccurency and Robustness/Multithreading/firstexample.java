3131f61478d854e0480483a588f92237a4f22876
Sarthakgiri455 <98268602+Sarthakgiri455@users.noreply.github.com>
2022-02-08 16:07:31 +0545

    Create firstexample.java

---
+++ Conccurency and Robustness/Multithreading/firstexample.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+
+class Multithread implements Runnable{
+	
+	public void run()//run lai explicitly invoke garna mildaina
+	{
+		System.out.println(Thread.currentThread() + " says hello");
+		for(int j=0;j<5;j++)
+		{
+		    System.out.print(j+"  ");
+		}
+		System.out.println();
+	}
+}
+class Multithread2 implements Runnable{
+	
+	public void run()//run lai explicitly invoke garna mildaina
+	{
+		System.out.println(Thread.currentThread() + " says hello");
+		System.out.println("VIT placements");
+		for(int j=-20;j<5;j++)
+		{
+		    System.out.print(j+"  ");
+		}
+		System.out.println();
+	}
+}
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Thread t1=new Thread(new Multithread());
+		Multithread2 m=new Multithread2();
+		Thread t2=new Thread(m);
+		
+		t1.start();
+		t2.start();
+		System.out.println(Thread.currentThread() + " says hello");
+	}
+}
+
+		
+		
+		
+		
